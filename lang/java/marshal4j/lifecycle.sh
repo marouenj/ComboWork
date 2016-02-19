321266d5f2680606e84c5d457da7fde64a3eff97
marouenj <marouen.jilani@gmail.com>
2016-02-19 18:51:24 +0900

    [marshal4j] fix script

--- lang/java/marshal4j/lifecycle.sh
+++ lang/java/marshal4j/lifecycle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name maven -v "$PWD:/usr/src/app" -v "$PWD/.m2:/root/.m2" marouenj/maven $1
+docker run --rm --name maven -v "$PWD:/files" -v "$PWD/.m2:/root/.m2" marouenj/maven $1
