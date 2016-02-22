8db00f5257be008b8fe7eb648a1138b50b40d265
marouenj <marouen.jilani@gmail.com>
2016-02-23 00:34:26 +0900

    [load4j] format docker script

--- lang/java/load4j/lifecycle.sh
+++ lang/java/load4j/lifecycle.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --rm --name maven -v "$PWD:/files" -v "$PWD/.m2:/root/.m2" marouenj/maven $1
+docker run \
+	--rm \
+	--name maven \
+	-v "$PWD:/files" \
+	-v "$PWD/.m2:/root/.m2" \
+marouenj/maven $1
