9b2cfd5c21563dc442e3e698054cecdc11f76456
marouenj <marouen.jilani@gmail.com>
2016-03-09 22:52:16 +0900

    [expect4j] add skeleton for validation

---
+++ lang/java/expect4j/src/main/java/combowork/expect4j/util/JsonUtil.java
@@ -0,0 +1,84 @@
+package combowork.expect4j.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Utility for {@link JsonNode}
+ *
+ * @author marouenj
+ */
+public class JsonUtil {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
+
+    private final static ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * List of log messages intrinsic to {@link JsonUtil}
+     */
+    private enum LogMessages {
+        UNABLE_TO_OPEN_FILE("Unable to open file, %s"),
+        UNABLE_TO_CONVERT_STREAM_TO_STRING("Unable to convert Stream to String in, %s"),
+        UNABLE_TO_PARSE_JSON("Unable to parse json tree. Json file is, %s");
+
+        private final String text;
+
+        LogMessages(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return this.text;
+        }
+
+        public String getText(String... vals) {
+            return String.format(getText(), vals);
+        }
+    }
+
+    public static JsonNode fromFile(File file) {
+        String path = file.getPath();
+        String text;
+        try {
+            text = IOUtils.toString(new FileReader(file));
+        } catch (IOException e) {
+            if (e instanceof FileNotFoundException) {
+                String msg = LogMessages.UNABLE_TO_OPEN_FILE.getText(path);
+                LOGGER.error(msg);
+                throw new RuntimeException(msg);
+            } else {
+                String msg = LogMessages.UNABLE_TO_CONVERT_STREAM_TO_STRING.getText(path);
+                LOGGER.error(msg);
+                throw new RuntimeException(msg);
+            }
+        }
+
+        return fromText(text, path);
+    }
+
+    private static JsonNode fromText(String text, String path) {
+        JsonNode tree;
+        try {
+            tree = MAPPER.readTree(text);
+        } catch (IOException e) {
+            String msg = LogMessages.UNABLE_TO_PARSE_JSON.getText(path);
+            LOGGER.error(msg);
+            throw new RuntimeException(msg);
+        }
+
+        return tree;
+    }
+
+    public static boolean isValid(JsonNode testcases) {
+        return true;
+    }
+}
