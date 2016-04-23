2fe5afd453caf1c9505934a73b561a7c82d20d1a
marouenj <marouen.jilani@gmail.com>
2016-04-23 23:18:16 +0900

    [filter] a decision test to decide whether to keep a test case or not

--- lang-agnostic/filter/filter_test.py
+++ lang-agnostic/filter/filter_test.py
@@ -3,6 +3,10 @@ import unittest
 import filter_util
 
 
+action_keep = "keep"
+action_discard = "discard"
+
+
 class FilterTest(unittest.TestCase):
 
     def test_matches(self):
@@ -21,6 +25,33 @@ class FilterTest(unittest.TestCase):
         for v in testcases:
             actual = filter_util.matches(v[0], v[1])
             self.assertEqual(v[2], actual)
+
+    def test_keep(self):
+        testcases = [
+            # test case 1
+            [[1, 2, 3], ["*", "*", "*"], action_keep, 1],
+            # test case 2
+            [[1, 2, 3], ["*", "*", "*"], action_discard, 0],
+            # test case 3
+            [[1, 2, 3], ["*", 2, "*"], action_keep, 1],
+            # test case 4
+            [[1, 2, 3], ["*", 2, "*"], action_discard, 0],
+            # test case 5
+            [[1, 2, 3], [1, 2, 4], action_keep, 0],
+            # test case 6
+            [[1, 2, 3], [1, 2, 4], action_discard, 1],
+            # test case 7
+            [[1, 2, 3], [1, 2, 3, 4], action_keep, 0],
+            # test case 8
+            [[1, 2, 3], [1, 2, 3, 4], action_discard, 1],
+            # test case 9
+            [[1, 2, 3, 4], [1, 2, 3], action_keep, 0],
+             # test case 10
+            [[1, 2, 3, 4], [1, 2, 3], action_discard, 1]
+        ]
+        for v in testcases:
+            actual = filter_util.keep(v[0], v[1], v[2])
+            self.assertEqual(v[3], actual)
 
 if __name__ == '__main__':
     unittest.main()
--- lang-agnostic/filter/filter_util.py
+++ lang-agnostic/filter/filter_util.py
@@ -1,3 +1,7 @@
+action_keep = "keep"
+action_discard = "discard"
+
+
 def matches(case, rule):
     """Match a test case against a rule
     return '1' if there's a match, 0 otherwise
@@ -11,3 +15,17 @@ def matches(case, rule):
             match = 0
             break
     return match
+
+
+def keep(case, rule, action):
+    """Decide to keep or discard the test case
+    Decision test depends on the result of 'matches' as well as the action (keep or discard)
+    """
+    keep = 0
+    if action == action_keep:
+        keep = 1 if matches(case, rule) else 0
+    elif action == action_discard:
+        keep = 0 if matches(case, rule) else 1
+    else:
+        keep = -1
+    return keep
