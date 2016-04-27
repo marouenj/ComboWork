0efc5aa87ef2670e2ab00b60e3b2a767fd9cbb68
marouenj <marouen.jilani@gmail.com>
2016-04-27 22:09:16 +0900

    [filter] format using the autopep8 docker image

--- lang-agnostic/filter/docker/fmt.sh
+++ lang-agnostic/filter/docker/fmt.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-for FILE in filter.py filter_util.py filter_test.py;
-do
 docker run \
        --rm \
-       --name fmt \
-       -v "$(pwd):/files" \
-combowork/filter autopep8 --in-place --aggressive --aggressive ${FILE}
-done
+       --name autopep8 \
+       -v $(pwd):/files \
+marouenj/autopep8
