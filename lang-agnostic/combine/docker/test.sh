a6ee0be8b8bdb8929d4fe28689fcb5dd1d37bf4a
marouenj <marouen.jilani@gmail.com>
2016-05-09 21:42:27 +0900

    [combine] add main package to test

--- lang-agnostic/combine/docker/test.sh
+++ lang-agnostic/combine/docker/test.sh
@@ -3,6 +3,8 @@
 docker run \
        --rm \
        --name go \
+       -v $(pwd)/combine.go:/go/src/github.com/marouenj/ComboWork/lang-agnostic/combine/combine.go \
+       -v $(pwd)/combine_test.go:/go/src/github.com/marouenj/ComboWork/lang-agnostic/combine/combine_test.go \
        -v $(pwd)/bitutil:/go/src/github.com/marouenj/ComboWork/lang-agnostic/combine/bitutil \
        -v $(pwd)/combiner:/go/src/github.com/marouenj/ComboWork/lang-agnostic/combine/combiner \
 golang:1.6 \
