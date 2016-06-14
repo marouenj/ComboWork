ddaf8bcd35ec292529709b7ec5fa58b40c8c66c5
marouenj <marouen.jilani@gmail.com>
2016-06-14 09:33:38 +0900

    [load4j] use vanilla maven for building

--- lang/java/load4j/lifecycle.sh
+++ lang/java/load4j/lifecycle.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+GOAL=test
+
+if [[ $1 != "" ]];
+then
+  GOAL=$1;
+fi
+
 docker run \
+       -it \
        --rm \
        --name maven \
        --net host \
-       -v "$(pwd):/files" \
+       -v "${PWD}":/files \
        -v "${HOME}/.m2:/root/.m2" \
-marouenj/maven $1
+       -w /files \
+maven:3-jdk-8 \
+mvn ${GOAL}
