33808950ba339fc5c4fcea60137f7961016955c5
marouenj <marouen.jilani@gmail.com>
2016-06-14 09:13:30 +0900

    [marshal4j] this package is deprecated

--- lang/java/marshal4j/docker_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t combowork/marshal4j .
--- lang/java/marshal4j/docker_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run \
-       --rm \
-       --name marshal4j \
-       -v $(pwd)/src/test/resources:/files \
-       combowork/marshal4j:latest \
-       --vars="vars.json" --vals="vars.json.vals" --out="vars.json.marshal4j"
